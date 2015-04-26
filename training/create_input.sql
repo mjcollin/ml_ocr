646db9fbace2beede591776e2ae53581ceebc97f
Matthew Collins <matt@badmatt.com>
2015-04-26 10:57:52 -0400

    Had some extra debugging columns

--- training/create_input.sql
+++ training/create_input.sql
@@ -13,8 +13,8 @@ CREATE TABLE
   input
 SELECT DISTINCT
   l.id, l.occid, l.text, l.label,
-  r.word_x0, r.word_y0, r.word_x1, r.word_y1,
-  e.min_word_y0, e.max_word_y1, 
+  -- r.word_x0, r.word_y0, r.word_x1, r.word_y1,
+  -- e.min_word_y0, e.max_word_y1, 
   (r.area_x0 - e.min_area_x0) / (e.max_area_x1 - e.min_area_x0) as scaled_area_x0,
   (r.area_y0 - e.min_area_y0) / (e.max_area_y1 - e.min_area_y0) as scaled_area_y0,
   (r.line_x0 - e.min_line_x0) / (e.max_line_x1 - e.min_line_x0) as scaled_line_x0,
