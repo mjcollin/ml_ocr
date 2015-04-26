2e22bf167e52a3b3695fb980781fcf37cf02e652
Matthew Collins <matt@badmatt.com>
2015-04-26 11:04:49 -0400

    Export inputs for things that read CSV

---
+++ training/export_input.sql
@@ -0,0 +1,13 @@
+/*
+Export what scikitlearn needs as CSV
+*/
+
+SELECT
+  id, label, scaled_area_x0, scaled_area_y0, 
+  scaled_line_x0, scaled_line_y0, scaled_word_x0, scaled_word_y0
+FROM
+  input
+INTO OUTFILE
+  '/tmp/input.csv'
+FIELDS TERMINATED BY ','
+;
