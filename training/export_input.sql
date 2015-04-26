554119e185e779e0217d9aa6a958c65809e7839f
Matthew Collins <matt@badmatt.com>
2015-04-26 11:47:38 -0400

    Added headers to CSV

--- training/export_input.sql
+++ training/export_input.sql
@@ -1,7 +1,10 @@
 /*
 Export what scikitlearn needs as CSV
 */
-
+SELECT
+  "id", "label", "scaled_area_x0", "scaled_area_y0",
+  "scaled_line_x0", "scaled_line_y0", "scaled_word_x0", "scaled_word_y0"
+UNION
 SELECT
   id, label, scaled_area_x0, scaled_area_y0, 
   scaled_line_x0, scaled_line_y0, scaled_word_x0, scaled_word_y0
